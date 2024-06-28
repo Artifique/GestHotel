134477678031021c4b711e9c90820b7bae13bf78
Fomba <fomba@gmail.com>
2024-06-29 00:29:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Application de gestion d'hotel
+==================================
+
+Introduction
+------------
+
+Cette application est pour la gestion d'un hotel, elle utilise les tehnologies suivantes:jetpack compose,
+ROOM 
+
+Pre-requies
+--------------
+
+Vous avez besoin de savoir:
+- Comment creer et utiliser les composables.
+- Comment naviguer entre eux et creer des data
+- Comment utiliser l'architecture ViewModel
+- Ce qu'est SQlite et comment creer des requêtes SQlites
+
+
+
+
---
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -0,0 +1,30 @@
+package com.example.inventory.data
+
+import androidx.compose.ui.graphics.Color
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+/**
+ * Entity data class represents a single row in the database.
+ */
+
+@Entity(tableName = "items")
+//Itm represente un element,ici une étage
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val price: Double,
+    val quantity: Int,
+    val selectedcount:Int=0
+)
+
+@Entity(tableName = "payhistory")
+data class history(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int=0,
+    val name:String,
+    val bill:Int,
+    val paid:Boolean=false
+)
